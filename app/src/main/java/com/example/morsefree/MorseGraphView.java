97d7e836afa538198252f4448b89fde3feaa878f
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-02-27 00:32:02 +0400

    intermediate

---
+++ app/src/main/java/com/example/morsefree/MorseGraphView.java
@@ -0,0 +1,13 @@
+package com.example.morsefree;
+
+import android.view.View;
+
+import android.content.Context;
+
+import android.util.AttributeSet;
+
+public class MorseGraphView extends View {
+    public MorseGraphView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+}
